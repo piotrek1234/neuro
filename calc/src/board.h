45ee07fe8d7781e9483050d7f464b7147ab1e435
piotrek1234 <pqcinski+gh@gmail.com>
2015-11-30 20:02:48 +0100

    doxygen config

--- calc/src/board.h
+++ calc/src/board.h
@@ -1,14 +1,6 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-#ifdef CALC_EXPORTS
-/** Workaround for Windows DLL library exports */
-#define CALC_DLL(X) __declspec(dllexport)X
-#else
-/** Workaround for Unix Shared Library exports */
-#define CALC_DLL(X) X
-#endif
-
 #include "token.h"
 #include "hex.h"
 #include <vector>
@@ -29,7 +21,7 @@ private:
         Hex(-1, 0, 1), Hex(-1, 1, 0), Hex(0, 1, -1)
     };
     int size;
-    map<Hex, Token*, > board;
+    std::map<Hex, Token*> board;
     
     
 };
